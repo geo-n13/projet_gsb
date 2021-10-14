5ab298c13412d9ea475529288cf71f48786327f6
Geo Mercore <geomercorepro@outlook.fr>
2021-10-14 16:38:46 +0200

    Completion of the following class : - VisiteDao.java - VisiteurDao.java

--- src/gsb/modele/dao/VisiteurDao.java
+++ src/gsb/modele/dao/VisiteurDao.java
@@ -1,7 +1,26 @@
 package gsb.modele.dao;
 
+import gsb.modele.Localite;
+import gsb.modele.Medicament;
+import gsb.modele.Visiteur;
+
+import java.sql.ResultSet;
+
 public class VisiteurDao {
-    /*
-    à compléter
-     */
+
+    public static Visiteur rechercher(String codeVisiteur) {
+        Visiteur unVisiteur = null;
+        ResultSet reqSelection = ConnexionMySql.execReqSelection("select * from VISITEUR where MATRICULE='"+codeVisiteur+"'");
+        try {
+            if (reqSelection.next()) {
+                unVisiteur = new Visiteur(reqSelection.getString(1), reqSelection.getString(2), reqSelection.getString(3), reqSelection.getString(4), reqSelection.getString(5), reqSelection.getString(6), LocaliteDao.rechercher(reqSelection.getString(7)), reqSelection.getString(8), reqSelection.getString(9), reqSelection.getInt(10), reqSelection.getString(11), reqSelection.getString(12));
+            }
+        }
+        catch (Exception e) {
+            System.out.println("erreur reqSelection.next() pour la requête - select * from VISITEUR where MATRICULE='"+codeVisiteur+"'");
+            e.printStackTrace();
+        }
+        ConnexionMySql.fermerConnexionBd();
+        return unVisiteur;
+    }
 }
