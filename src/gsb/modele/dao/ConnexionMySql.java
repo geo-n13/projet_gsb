8a5144fc2bccf2ae1e85f5d9951b64ce2f17a388
Geo Mercore <geomercorepro@outlook.fr>
2021-10-14 17:16:00 +0200

    Modification of the ConnexionMySql.java file

--- src/gsb/modele/dao/ConnexionMySql.java
+++ src/gsb/modele/dao/ConnexionMySql.java
@@ -33,10 +33,10 @@ public class ConnexionMySql { // DAO = Data Access Object
      */
     public static void connecterBd(){
         //connexion à la base de donnée à partir de jdbc
-        String url = "jdbc:mysql://192.168.60.62:3306/gsb"; // url : chaine de connexion
+        String url = "jdbc:mysql://192.172.1.13:3306/GSB"; // url : chaine de connexion
         // try permet d'essayer de lancer la connexion
-        try {Class.forName("com.mysql.jdbc.Driver");
-            cnx = DriverManager.getConnection(url,"usergsb","password");
+        try {Class.forName("com.mysql.cj.jdbc.Driver");
+            cnx = DriverManager.getConnection(url,"admindb","password");
         }
         // si la connexion echoue un message d'erreur est affiché
         catch(Exception e) {  System.out.println("Echec lors de la connexion");  }
